35e3e4577cc2e64ca0e1cf41cd88e9a4062f4d40
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-07-19 21:49:58 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1719,17 +1719,6 @@ gui:AddButton({
     end
 })
 
-Doors:AddLabel({ Name = "Can send Message enity:" })
-Doors:AddLabel({ Name = "Rush Ambush" })
-Doors:AddLabel({ Name = "Snare A60" })
-Doors:AddLabel({ Name = "A90 A120 Eyes" })
-Doors:AddLabel({ Name = "-------------------------" })
-Doors:AddLabel({ Name = "Can esp enity:" })
-Doors:AddLabel({ Name = "Rush Ambush" })
-Doors:AddLabel({ Name = "Snare A60" })
-Doors:AddLabel({ Name = "A120 Eyes" })
-Doors:AddLabel({ Name = "Tip:有些功能可能失效" })
-
 local a90remote = game.ReplicatedStorage:WaitForChild("EntityInfo"):WaitForChild("A90")
 local plr = game.Players.LocalPlayer
 local flags = {noa90 = false}
@@ -1940,196 +1929,6 @@ local keyESPToggle = Doors:AddToggle({
     end
 })
 
-local chestESPToggle = Doors:AddToggle({
-    Name = "Chest ESP",
-    Default = false,
-    Callback = function(state)
-        if state then
-            _G.chestESPInstances = {}
-            local esptable = {chests = {}}
-            local esptableinstances = {}
-            local flags = {espchest = true}
-
-            local function esp(what, color, core, name)
-                local parts
-                
-                if typeof(what) == "Instance" then
-                    if what:IsA("Model") then
-                        parts = what:GetChildren()
-                    elseif what:IsA("BasePart") then
-                        parts = {what, table.unpack(what:GetChildren())}
-                    end
-                elseif typeof(what) == "table" then
-                    parts = what
-                end
-                
-                local bill
-                local boxes = {}
-                
-                for i, v in pairs(parts) do
-                    if v:IsA("BasePart") then
-                        local box = Instance.new("BoxHandleAdornment")
-                        box.Size = v.Size
-                        box.AlwaysOnTop = true
-                        box.ZIndex = 1
-                        box.AdornCullingMode = Enum.AdornCullingMode.Never
-                        box.Color3 = color
-                        box.Transparency = 1
-                        box.Adornee = v
-                        box.Parent = game.CoreGui
-                        
-                        table.insert(boxes, box)
-                        
-                        task.spawn(function()
-                            while box do
-                                if box.Adornee == nil or not box.Adornee:IsDescendantOf(workspace) then
-                                    box.Adornee = nil
-                                    box.Visible = false
-                                    box:Destroy()
-                                end  
-                                task.wait()
-                            end
-                        end)
-                    end
-                end
-                
-                if core and name then
-                    bill = Instance.new("BillboardGui", game.CoreGui)
-                    bill.AlwaysOnTop = true
-                    bill.Size = UDim2.new(0, 400, 0, 100)
-                    bill.Adornee = core
-                    bill.MaxDistance = 2000
-                    
-                    local mid = Instance.new("Frame", bill)
-                    mid.AnchorPoint = Vector2.new(0.5, 0.5)
-                    mid.BackgroundColor3 = color
-                    mid.Size = UDim2.new(0, 8, 0, 8)
-                    mid.Position = UDim2.new(0.5, 0, 0.5, 0)
-                    Instance.new("UICorner", mid).CornerRadius = UDim.new(1, 0)
-                    Instance.new("UIStroke", mid)
-                    
-                    local txt = Instance.new("TextLabel", bill)
-                    txt.AnchorPoint = Vector2.new(0.5, 0.5)
-                    txt.BackgroundTransparency = 1
-                    txt.BackgroundColor3 = color
-                    txt.TextColor3 = color
-                    txt.Size = UDim2.new(1, 0, 0, 20)
-                    txt.Position = UDim2.new(0.5, 0, 0.7, 0)
-                    txt.Text = name
-                    Instance.new("UIStroke", txt)
-                    
-                    task.spawn(function()
-                        while bill do
-                            if bill.Adornee == nil or not bill.Adornee:IsDescendantOf(workspace) then
-                                bill.Enabled = false
-                                bill.Adornee = nil
-                                bill:Destroy() 
-                            end  
-                            task.wait()
-                        end
-                    end)
-                end
-                
-                local ret = {}
-                
-                ret.delete = function()
-                    for i, v in pairs(boxes) do
-                        v.Adornee = nil
-                        v.Visible = false
-                        v:Destroy()
-                    end
-                    
-                    if bill then
-                        bill.Enabled = false
-                        bill.Adornee = nil
-                        bill:Destroy() 
-                    end
-                end
-                
-                return ret 
-            end
-
-            local function check(v, room)
-                task.wait()
-                if table.find(esptableinstances, v) then
-                    return
-                end
-
-                if v:IsA("Model") then
-                    local okvaluechange = nil
-                    if v.Name == "ChestBox" then
-                        warn(v.Name)
-                        local h = esp(v, Color3.fromRGB(205, 120, 255), v.PrimaryPart, "Chest")
-                        table.insert(esptable.chests, h) 
-                        table.insert(esptableinstances, v)
-                        okvaluechange = game:GetService("ReplicatedStorage").GameData.LatestRoom:GetPropertyChangedSignal("Value"):Connect(function()
-                            if tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value) ~= room.Name then
-                                h.delete()
-                                okvaluechange:Disconnect()
-                            end
-                        end)
-                    elseif v.Name == "ChestBoxLocked" then
-                        local h = esp(v, Color3.fromRGB(255, 120, 205), v.PrimaryPart, "Locked Chest")
-                        table.insert(esptable.chests, h) 
-                        table.insert(esptableinstances, v)
-                        okvaluechange = game:GetService("ReplicatedStorage").GameData.LatestRoom:GetPropertyChangedSignal("Value"):Connect(function()
-                            if tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value) ~= room.Name then
-                                h.delete()
-                                okvaluechange:Disconnect()
-                            end
-                        end)
-                    end
-                end
-	    end
-
-	    local function setup(room)
-                task.wait(0.1)
-                local subaddcon
-                subaddcon = room.DescendantAdded:Connect(function(v)
-                    check(v, room) 
-                end)
-
-                for i, v in pairs(room:GetDescendants()) do
-                    check(v, room)
-                end
-
-                task.spawn(function()
-                    repeat task.wait() until not flags.espchest
-                    subaddcon:Disconnect()  
-                end)  
-            end
-
-            local addconnect
-            addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
-                setup(room)
-            end)
-
-            for i, room in pairs(workspace.CurrentRooms:GetChildren()) do
-                if room:FindFirstChild("Assets") then
-                    setup(room) 
-                end
-                task.wait()
-            end
-
-            if workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:FindFirstChild("Assets") then
-                setup(workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)])
-            end
-
-            table.insert(_G.chestESPInstances, esptable)
-
-        else
-            if _G.chestESPInstances then
-                for _, instance in pairs(_G.chestESPInstances) do
-                    for _, v in pairs(instance.chests) do
-                        v.delete()
-                    end
-                end
-                _G.chestESPInstances = nil
-            end
-        end
-    end
-})
-
 local removeObstructionsToggle = Doors:AddToggle({
     Name = "Remove Obstructions",
     Default = false,
@@ -2152,7 +1951,7 @@ local removeObstructionsToggle = Doors:AddToggle({
             task.spawn(function()
                 while _G.removeObstructionsEnabled do
                     removeObstructions()
-                    task.wait(1)  -- 每秒检查一次
+                    task.wait(1)
                 end
             end)
         else
@@ -2160,3 +1959,14 @@ local removeObstructionsToggle = Doors:AddToggle({
         end
     end
 })
+
+Doors:AddLabel({ Name = "Can send Message enity:" })
+Doors:AddLabel({ Name = "Rush Ambush" })
+Doors:AddLabel({ Name = "Snare A60" })
+Doors:AddLabel({ Name = "A90 A120 Eyes" })
+Doors:AddLabel({ Name = "-------------------------" })
+Doors:AddLabel({ Name = "Can esp enity:" })
+Doors:AddLabel({ Name = "Rush Ambush" })
+Doors:AddLabel({ Name = "Snare A60" })
+Doors:AddLabel({ Name = "A120 Eyes" })
+Doors:AddLabel({ Name = "Tip:有些功能可能失效" })
