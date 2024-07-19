e39502031d7bc3271bb206573139c9532b5cd2f4
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-07-19 10:45:14 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -648,6 +648,238 @@ local playerESP = Doors:AddToggle({
         end
     end
 })
+
+local itemESPToggle = Doors:AddToggle({
+    Name = "Item ESP",
+    Default = false,
+    Callback = function(state)
+        if state then
+            _G.espInstances = {}
+            local esptable = {items = {}}
+            local flags = {espitems = true}
+
+            local function esp(what, color, core, name)
+                local parts
+                
+                if typeof(what) == "Instance" then
+                    if what:IsA("Model") then
+                        parts = what:GetChildren()
+                    elseif what:IsA("BasePart") then
+                        parts = {what, table.unpack(what:GetChildren())}
+                    end
+                elseif typeof(what) == "table" then
+                    parts = what
+                end
+                
+                local bill
+                local boxes = {}
+                
+                for i, v in pairs(parts) do
+                    if v:IsA("BasePart") then
+                        local box = Instance.new("BoxHandleAdornment")
+                        box.Size = v.Size
+                        box.AlwaysOnTop = true
+                        box.ZIndex = 1
+                        box.AdornCullingMode = Enum.AdornCullingMode.Never
+                        box.Color3 = color
+                        box.Transparency = 1
+                        box.Adornee = v
+                        box.Parent = game.CoreGui
+                        
+                        table.insert(boxes, box)
+                        
+                        task.spawn(function()
+                            while box do
+                                if box.Adornee == nil or not box.Adornee:IsDescendantOf(workspace) then
+                                    box.Adornee = nil
+                                    box.Visible = false
+                                    box:Destroy()
+                                end  
+                                task.wait()
+                            end
+                        end)
+                    end
+                end
+                
+                if core and name then
+                    bill = Instance.new("BillboardGui", game.CoreGui)
+                    bill.AlwaysOnTop = true
+                    bill.Size = UDim2.new(0, 400, 0, 100)
+                    bill.Adornee = core
+                    bill.MaxDistance = 2000
+                    
+                    local mid = Instance.new("Frame", bill)
+                    mid.AnchorPoint = Vector2.new(0.5, 0.5)
+                    mid.BackgroundColor3 = color
+                    mid.Size = UDim2.new(0, 8, 0, 8)
+                    mid.Position = UDim2.new(0.5, 0, 0.5, 0)
+                    Instance.new("UICorner", mid).CornerRadius = UDim.new(1, 0)
+                    Instance.new("UIStroke", mid)
+                    
+                    local txt = Instance.new("TextLabel", bill)
+                    txt.AnchorPoint = Vector2.new(0.5, 0.5)
+                    txt.BackgroundTransparency = 1
+                    txt.BackgroundColor3 = color
+                    txt.TextColor3 = color
+                    txt.Size = UDim2.new(1, 0, 0, 20)
+                    txt.Position = UDim2.new(0.5, 0, 0.7, 0)
+                    txt.Text = name
+                    Instance.new("UIStroke", txt)
+                    
+                    task.spawn(function()
+                        while bill do
+                            if bill.Adornee == nil or not bill.Adornee:IsDescendantOf(workspace) then
+                                bill.Enabled = false
+                                bill.Adornee = nil
+                                bill:Destroy() 
+                            end  
+                            task.wait()
+                        end
+                    end)
+                end
+                
+                local ret = {}
+                
+                ret.delete = function()
+                    for i, v in pairs(boxes) do
+                        v.Adornee = nil
+                        v.Visible = false
+                        v:Destroy()
+                    end
+                    
+                    if bill then
+                        bill.Enabled = false
+                        bill.Adornee = nil
+                        bill:Destroy() 
+                    end
+                end
+                
+                return ret 
+            end
+
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                local assets = room:FindFirstChild("Assets")
+                if assets then
+                    for _, v in pairs(assets:GetDescendants()) do
+                        if v:IsA("Model") and (v:GetAttribute("Pickup") or v:GetAttribute("PropType")) then
+                            task.wait(0.1)
+                            local part = v:FindFirstChild("Handle") or v:FindFirstChild("Prop")
+                            if part then
+                                local h = esp(part, Color3.fromRGB(255, 255, 255), part, v.Name)
+                                table.insert(esptable.items, h)
+                            end
+                        end
+                    end)
+                end
+            end
+
+            workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                local assets = room:WaitForChild("Assets")
+                if assets then
+                    for _, v in pairs(assets:GetDescendants()) do
+                        if v:IsA("Model") and (v:GetAttribute("Pickup") or v:GetAttribute("PropType")) then
+                            task.wait(0.1)
+                            local part = v:FindFirstChild("Handle") or v:FindFirstChild("Prop")
+                            if part then
+                                local h = esp(part, Color3.fromRGB(255, 255, 255), part, v.Name)
+                                table.insert(esptable.items, h)
+                            end
+                        end
+		    end
+			
+		    assets.DescendantAdded:Connect(function(v)
+                        if v:IsA("Model") and (v:GetAttribute("Pickup") or v:GetAttribute("PropType")) then
+                            task.wait(0.1)
+                            local part = v:FindFirstChild("Handle") or v:FindFirstChild("Prop")
+                            if part then
+                                local h = esp(part, Color3.fromRGB(255, 255, 255), part, v.Name)
+                                table.insert(esptable.items, h)
+                            end
+                        end
+                    end)
+                end
+            end)
+
+            table.insert(_G.espInstances, esptable)
+
+        else
+            if _G.espInstances then
+                for _, instance in pairs(_G.espInstances) do
+                    for _, v in pairs(instance.items) do
+                        v.delete()
+                    end
+                end
+                _G.espInstances = nil
+            end
+        end
+    end
+})
+
+local libraryCodeToggle = Doors:AddToggle({
+    Name = "Library Code ESP",
+    Default = false,
+    Callback = function(state)
+        if state then
+            _G.codeInstances = {}
+            local char = game.Players.LocalPlayer.Character
+            local plr = game.Players.LocalPlayer
+
+            local function notify(title, description)
+                Notification:Notify(
+                    {Title = title, Description = description},
+                    {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+                    {Image = "http://www.roblox.com/asset/?id=10802751252", ImageColor = Color3.fromRGB(255, 255, 255)}
+                )
+            end
+
+            local function deciphercode()
+                local paper = char:FindFirstChild("LibraryHintPaper")
+                local hints = plr.PlayerGui:WaitForChild("PermUI"):WaitForChild("Hints")
+                
+                local code = {[1]="_", [2]="_", [3]="_", [4]="_", [5]="_"}
+                
+                if paper then
+                    for _, v in pairs(paper:WaitForChild("UI"):GetChildren()) do
+                        if v:IsA("ImageLabel") and v.Name ~= "Image" then
+                            for _, img in pairs(hints:GetChildren()) do
+                                if img:IsA("ImageLabel") and img.Visible and v.ImageRectOffset == img.ImageRectOffset then
+                                    local num = img:FindFirstChild("TextLabel").Text
+                                    code[tonumber(v.Name)] = num 
+                                end
+                            end
+                        end
+                    end 
+                end
+                
+                return code
+            end
+
+            local addconnect
+            addconnect = char.ChildAdded:Connect(function(v)
+                if v:IsA("Tool") and v.Name == "LibraryHintPaper" then
+                    task.wait()
+                    
+                    local code = table.concat(deciphercode())
+                    
+                    if code:find("_") then
+                        notify("Creepy client V2", "You need go to collect book")
+                    else
+                        notify("Creepy client V2", "Code = " .. code)
+                    end
+                end
+            end)
+
+            table.insert(_G.codeInstances, addconnect)
+        else
+            if _G.codeInstances then
+                for _, instance in pairs(_G.codeInstances) do
+                    instance:Disconnect()
+                end
+                _G.codeInstances = nil
+            end
+        end
+    end
+})
 Doors:AddLabel({ Name = "Can send Message enity:" })
 Doors:AddLabel({ Name = "Rush Ambush" })
 Doors:AddLabel({ Name = "Snare A60" })
