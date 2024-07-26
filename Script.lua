046e61eee38a2bdd14a7ea953c47185b46e6fe0e
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-07-26 17:25:11 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2961,6 +2961,81 @@ local playerESP = a:AddToggle({
         end
     end
 })
+local lockerESPToggle = Pressure:AddToggle({
+    Name = "BigRoomDoor ESP",
+    Default = false,
+    Callback = function(state)
+        if state then
+            _G.bigRoomDoorESPInstances = {}
+            local esptable = {doors = {}}
+
+            local function createBillboard(instance, name, color)
+                local bill = Instance.new("BillboardGui", game.CoreGui)
+                bill.AlwaysOnTop = true
+                bill.Size = UDim2.new(0, 100, 0, 50)
+                bill.Adornee = instance
+                bill.MaxDistance = 2000
+
+                local mid = Instance.new("Frame", bill)
+                mid.AnchorPoint = Vector2.new(0.5, 0.5)
+                mid.BackgroundColor3 = color
+                mid.Size = UDim2.new(0, 8, 0, 8)
+                mid.Position = UDim2.new(0.5, 0, 0.5, 0)
+                Instance.new("UICorner", mid).CornerRadius = UDim.new(1, 0)
+                Instance.new("UIStroke", mid)
+
+                local txt = Instance.new("TextLabel", bill)
+                txt.AnchorPoint = Vector2.new(0.5, 0.5)
+                txt.BackgroundTransparency = 1
+                txt.TextColor3 = color
+                txt.Size = UDim2.new(1, 0, 0, 20)
+                txt.Position = UDim2.new(0.5, 0, 0.7, 0)
+                txt.Text = name
+                Instance.new("UIStroke", txt)
+
+                task.spawn(function()
+                    while bill do
+                        if bill.Adornee == nil or not bill.Adornee:IsDescendantOf(workspace) then
+                            bill.Enabled = false
+                            bill.Adornee = nil
+                            bill:Destroy()
+                        end
+                        task.wait()
+                    end
+                end)
+            end
+
+            local function monitorBigRoomDoor()
+                for _, instance in pairs(workspace:GetDescendants()) do
+                    if instance:IsA("Model") and instance.Name == "BigRoomDoor" then
+                        createBillboard(instance, "BigRoomDoor", Color3.new(125, 125, 125)) -- Change color as needed
+                    end
+                end
+
+                workspace.DescendantAdded:Connect(function(instance)
+                    if instance:IsA("Model") and instance.Name == "BigRoomDoor" then
+                        createBillboard(instance, "BigRoomDoor", Color3.new(125, 125, 125)) -- Change color as needed
+                    end
+                end)
+            end
+
+            monitorBigRoomDoor()
+            table.insert(_G.bigRoomDoorESPInstances, esptable)
+                
+        else
+            if _G.bigRoomDoorESPInstances then
+                for _, instance in pairs(_G.bigRoomDoorESPInstances) do
+                    for _, v in pairs(instance.doors) do
+                        if v:FindFirstChild("BillboardGui") then
+                            v.BillboardGui:Destroy()
+                        end
+                    end
+                end
+                _G.bigRoomDoorESPInstances = nil
+            end
+        end
+    end
+})
 
 local b = GUI:CreateSection({
     Name = "使用提示"
