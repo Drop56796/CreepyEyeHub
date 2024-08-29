759cea79f5e312da661df4bb9b46f9a2032dea7c
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-08-29 10:18:08 +0800

    Create Hydraulic Doors.lua

---
+++ Hydraulic Doors.lua
@@ -0,0 +1,232 @@
+local repo = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/'
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local v = 2
+local Players = game:GetService("Players")
+local textChannel = game:GetService("TextChatService"):WaitForChild("TextChannels"):WaitForChild("RBXGeneral")
+local player = Players.LocalPlayer
+local RunService = game:GetService("RunService")
+local char = player.Character or player.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")  -- Ensure Humanoid exists
+local rootPart = char:WaitForChild("HumanoidRootPart")
+--------A1000↓---------------------
+--local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+function warnNofiy(title, text)
+	Notification:Notify(
+		{Title = title, Description = text},
+		{OutlineColor = Color3.fromRGB(80, 80, 80),Time = timee or 5, Type = "image"},
+		{Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 0, 0)}
+	)
+end
+function Nofiy(title, text)
+	Notification:Notify(
+		{Title = title, Description = text},
+		{OutlineColor = Color3.fromRGB(80, 80, 80),Time = timee or 5, Type = "image"},
+		{Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 255, 255)}
+	)
+end
+--function newNofiy(title, text, text2, id)
+--    achievementGiver({
+--        Title = title,   
+--        Desc = text,     
+--        Reason = text2,  
+--        Image = id 
+--    })
+--end
+
+----- 示例调用 NewNotify 函数
+--newNofiy("Hydraulic Doors", "hi", "Welcome to use", "rbxassetid://12309073114")
+local buttons = {
+    tpwalktoggle = nil,  -- TP Walk 开关按钮
+    tpwalkspeed = nil,   -- TP Walk 速度滑块
+    camfov = nil,   -- FOV 滑块
+    noclip = nil,
+    noseek = nil
+}
+
+local flags = {
+    tpwalktoggle = false,  -- TP Walk 开关标志
+    tpwalkspeed = 16,      -- TP Walk 速度标志
+    camfov = 70,           -- FOV 标志
+    camfovtoggle = false,  -- FOV 开关标志（添加这个以匹配开关功能）
+    esprush = false,
+    espdoors = false,
+    esplocker = false,
+    espitems = false,
+    espbooks = false,
+    espgold = false,
+    targetKeyObtain = false,
+    noclip = false,
+    getcode = false,
+    itemaura = false,
+    error = false,
+    noa90 = false,
+    noseek = false,
+    esploc = false
+}
+local esptable = {
+    entity = {},
+	  doors = {},
+	  lockers = {},
+	  items = {},
+	  books = {},
+	  Gold = {},
+	  key = {},
+	  loc = {}
+}
+
+local Window = Library:CreateWindow({
+    Title = 'Hydraulic Doors' .. v,
+    Center = true,
+    AutoShow = true,
+    Resizable = true,
+    ShowCustomCursor = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['UI Settings'] = Window:AddTab('UI Settings'),
+}
+
+local MainGroup = Tabs.Main:AddLeftGroupbox('player')
+local OtherGroup = Tabs.Other:AddLeftGroupbox('Other Hub')
+local RightGroup = Tabs.Main:AddRightGroupbox('esp')
+
+Library:SetWatermarkVisibility(true)
+
+-- Example of dynamically-updating watermark with common traits (fps and ping)
+local FrameTimer = tick()
+local FrameCounter = 0;
+local FPS = 60;
+
+local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function()
+	FrameCounter += 1;
+
+	if (tick() - FrameTimer) >= 1 then
+		FPS = FrameCounter;
+		FrameTimer = tick();
+		FrameCounter = 0;
+	end;
+
+	Library:SetWatermark(('Doors ' .. v ' | %s fps | %s ms'):format(
+		math.floor(FPS),
+		math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
+	));
+end);
+
+Library.KeybindFrame.Visible = true; -- todo: add a function for this
+
+Library:OnUnload(function()
+	WatermarkConnection:Disconnect()
+
+	print('Unloaded!')
+	Library.Unloaded = true
+end)
+
+-- UI Settings
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+-- I set NoUI so it does not show up in the keybinds menu
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+
+Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- ThemeManager (Allows you to have a menu theme system)
+
+-- Hand the library over to our managers
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- Adds our MenuKeybind to the ignore list
+-- (do you want each config to have a different menu key? probably not.)
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+ThemeManager:SetFolder('MyScriptHub')
+SaveManager:SetFolder('MyScriptHub/specific-game')
+
+-- Builds our config menu on the right side of our tab
+SaveManager:BuildConfigSection(Tabs['UI Settings'])
+
+-- Builds our theme menu (with plenty of built in themes) on the left side
+-- NOTE: you can also call ThemeManager:ApplyToGroupbox to add it to a specific groupbox
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
+
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
+
+local RunService = game:GetService("RunService")
+LeftGroupBox:AddSlider('Speed', {
+	Text = 'Speed',
+	Default = 20,
+	Min = 20,
+	Max = 22,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(val)
+		flags.tpwalkspeed = val
+        if flags.tpwalktoggle then
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val  -- Directly set WalkSpeed if toggle is on
+        end
+    end
+})
+buttons.tpwalkspeed = tpwalkspeedslider
+MainGroup:AddToggle('ToggleSpeed', {
+    Text = 'Toggle Speed',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(val)
+        flags.tpwalktoggle = val
+        if val then
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = flags.tpwalkspeed  -- Apply selected WalkSpeed when enabled
+        else
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16  -- Reset to default WalkSpeed when disabled
+        end
+    end
+})
+buttons.tpwalktoggle = tpwalktglbtn
+MainGroup:AddLabel('-----------------------', true)
+MainGroup:AddToggle('No Clip', {
+    Text = 'No Clip',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(Value)
+        local player = game.Players.LocalPlayer
+        local char = player.Character
+        local runService = game:GetService("RunService")
+        if Value then
+            _G.NoClip = runService.Stepped:Connect(function()
+                for _, v in pairs(char:GetDescendants()) do
+                    if v:IsA("BasePart") then
+                        v.CanCollide = false
+                    end
+                end
+            end)
+        else
+            if _G.NoClip then
+                _G.NoClip:Disconnect()
+                _G.NoClip = nil
+            end
+            for _, v in pairs(char:GetDescendants()) do
+                if v:IsA("BasePart") then
+                    v.CanCollide = true
+                end
+            end
+        end
+    end
+})
