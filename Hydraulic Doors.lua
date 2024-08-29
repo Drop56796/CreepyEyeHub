7e074f0f1f653c013520e5bd760a9271a0a7d555
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-08-29 19:48:58 +0800

    Update Hydraulic Doors.lua

--- Hydraulic Doors.lua
+++ Hydraulic Doors.lua
@@ -177,7 +177,7 @@ MainGroup:AddSlider('Speed', {
 	Rounding = 1,
 	Compact = false,
 
-	Callback = function(val)
+	Callback = function(val, oldval)
 	flags.tpwalkspeed = val
         if flags.tpwalktoggle then
             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val  -- Directly set WalkSpeed if toggle is on
@@ -189,7 +189,7 @@ MainGroup:AddToggle('ToggleSpeed', {
     Text = 'Toggle Speed',
     Default = false,
     Tooltip = 'Walk through walls',
-    Callback = function(val)
+    Callback = function(val, oldval)
         flags.tpwalktoggle = val
         if val then
             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = flags.tpwalkspeed  -- Apply selected WalkSpeed when enabled
@@ -199,7 +199,48 @@ MainGroup:AddToggle('ToggleSpeed', {
     end
 })
 buttons.tpwalktoggle = tpwalktglbtn
-MainGroup:AddLabel('-----------------------', true)
+MainGroup:AddSlider('fov', {
+	Text = 'FOV',
+	Default = 70,
+	Min = 70,
+	Max = 120,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(val, oldval)
+	flags.camfov = val
+    end
+})
+buttons.camfov = camfovslider
+MainGroup:AddToggle('Togglefov', {
+    Text = 'Toggle FOV',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(val, oldval)
+        flags.camfovtoggle = val
+        if not val then
+            waitframes(2)
+            game:GetService("Workspace").CurrentCamera.FieldOfView = 70
+        end
+   end
+})
+buttons.camfovtoggle = togglefovbtn
+
+task.spawn(function()
+    RunService.RenderStepped:Connect(function()
+        if not rootPart or not hum then return end
+
+        -- Update FOV
+        if flags.camfovtoggle then
+            pcall(function()
+                game:GetService("Workspace").CurrentCamera.FieldOfView = flags.camfov
+            end)
+        end
+    end)
+end)
+
+
+MainGroup:AddLabel('---------------------', true)
 MainGroup:AddToggle('No Clip', {
     Text = 'No Clip',
     Default = false,
@@ -226,6 +267,408 @@ MainGroup:AddToggle('No Clip', {
                     v.CanCollide = true
                 end
             end
+        end
+    end
+})
+RightGroup:AddToggle('pe', {
+    Text = 'Player esp',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            _G.espInstances = {}
+            for _, player in pairs(game.Players:GetPlayers()) do
+                if player.Character then
+                    local espInstance = esp(player.Character, Color3.new(1, 0.5, 0), player.Character:FindFirstChild("HumanoidRootPart"), player.Name)
+                    table.insert(_G.espInstances, espInstance)
+                end
+            end
+        else
+            if _G.espInstances then
+                for _, espInstance in pairs(_G.espInstances) do
+                    espInstance.delete()
+                end
+                _G.espInstances = nil
+            end
+        end
+    end
+})
+
+RightGroup:AddToggle('ee', {
+    Text = 'enity esp',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            _G.entityESPInstances = {}
+            flags.esprush = state
+            local entitynames = {"RushMoving", "AmbushMoving", "Snare", "A60", "A120", "Eyes", "JeffTheKiller", "SeekMoving"}
+	    
+            local addconnect
+            addconnect = workspace.ChildAdded:Connect(function(v)
+                if table.find(entitynames, v.Name) then
+                    task.wait(0.1)
+                    
+                    local h = esp(v, Color3.fromRGB(255, 25, 25), v.PrimaryPart, v.Name:gsub("Moving", ""))
+                    table.insert(esptable.entity, h)
+                end
+            end)
+
+            local function setup(room)
+                if room.Name == "50" or room.Name == "100" then
+                    local figuresetup = room:WaitForChild("FigureSetup")
+                
+                    if figuresetup then
+                        local fig = figuresetup:WaitForChild("FigureRagdoll")
+                        task.wait(0.1)
+                        
+                        local h = esp(fig, Color3.fromRGB(255, 25, 25), fig.PrimaryPart, "Figure")
+                        table.insert(esptable.entity, h)
+                    end 
+                else
+                    local assets = room:WaitForChild("Assets")
+                    
+                    local function check(v)
+                        if v:IsA("Model") and table.find(entitynames, v.Name) then
+                            task.wait(0.1)
+                            
+                            local h = esp(v:WaitForChild("Base"), Color3.fromRGB(255, 25, 25), v.Base, "Snare")
+                            table.insert(esptable.entity, h)
+                        end
+                    end
+                    
+                    assets.DescendantAdded:Connect(function(v)
+                        check(v) 
+                    end)
+                    
+                    for i, v in pairs(assets:GetDescendants()) do
+                        check(v)
+                    end
+                end 
+            end
+            
+            local roomconnect
+            roomconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                setup(room)
+            end)
+            
+            for i, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                setup(room) 
+	    end
+
+	    table.insert(_G.entityESPInstances, esptable)
+
+        else
+            if _G.entityESPInstances then
+                for _, instance in pairs(_G.entityESPInstances) do
+                    for _, v in pairs(instance.entity) do
+                        v.delete()
+                    end
+                end
+                _G.entityESPInstances = nil
+            end
+        end
+    end
+})
+
+RightGroup:AddToggle('pe', {
+    Text = 'Door esp',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            _G.doorESPInstances = {}
+            flags.espdoors = state
+            local doorCounter = 0  -- Initialize a counter for the doors
+                
+            local function setup(room)
+                local door = room:WaitForChild("Door") -- Directly get the Door object
+                
+                task.wait(0.1)
+                
+                -- Increment the door counter and format it as a four-digit number starting from 0001
+                doorCounter = doorCounter + 1
+                local doorIndex = string.format("%04d", doorCounter)
+                
+                -- Set up ESP with the door index in the format "Door [0001]"
+                local h = esp(door:WaitForChild("Door"), Color3.fromRGB(90, 255, 40), door, "Door [" .. doorIndex .. "]")
+                table.insert(esptable.doors, h)
+                
+                door:WaitForChild("Door"):WaitForChild("Open").Played:Connect(function()
+                    h.delete()
+                end)
+                
+                door.AncestryChanged:Connect(function()
+                    h.delete()
+                end)
+            end
+            
+            local addconnect
+            addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                setup(room)
+            end)
+            
+            for i, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room:FindFirstChild("Assets") then
+                    setup(room) 
+                end
+            end
+
+            table.insert(_G.doorESPInstances, esptable)
+
+        else
+            if _G.doorESPInstances then
+                for _, instance in pairs(_G.doorESPInstances) do
+                    for _, v in pairs(instance.doors) do
+                        v.delete()
+                    end
+                end
+                _G.doorESPInstances = nil
+            end
+        end
+    end
+})
+
+MainGroup:AddToggle('pe', {
+    Text = 'Full bright',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        local Light = game:GetService("Lighting")
+
+        local function dofullbright()
+            Light.Ambient = Color3.new(1, 1, 1)
+            Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+            Light.ColorShift_Top = Color3.new(1, 1, 1)
+        end
+
+        local function resetLighting()
+            Light.Ambient = Color3.new(0, 0, 0)
+            Light.ColorShift_Bottom = Color3.new(0, 0, 0)
+            Light.ColorShift_Top = Color3.new(0, 0, 0)
+        end
+
+        if state then
+            _G.fullBrightEnabled = true
+            task.spawn(function()
+                while _G.fullBrightEnabled do
+                    dofullbright()
+                    task.wait(0)  -- 每秒检查一次
+                end
+            end)
+        else
+            _G.fullBrightEnabled = false
+            resetLighting()
+        end
+    end
+})
+
+MainGroup:AddLabel('---------------------')
+MainGroup:AddToggle('pe', {
+    Text = 'Enity Event',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            local entityNames = {"RushMoving", "AmbushMoving", "Snare", "A60", "A120", "A90", "Eyes", "JeffTheKiller"}  -- 实体名称
+
+            -- 确保 flags 和 plr 已定义
+            local flags = flags or {} -- 防止错误
+            local plr = game.Players.LocalPlayer -- 防止错误2
+
+            local function notifyEntitySpawn(entity)
+                local entityMessage
+                if entity.Name:gsub("Moving", ""):lower() == "Jeffthekiller" then
+                    entityMessage = "Entity Event: JeffTheKiller in the next door and be careful of his attack."
+                else
+                    entityMessage = "Entity Event: " .. entity.Name:gsub("Moving", ""):lower() .. " Spawned!"
+                end
+                Library:Notify(entityMessage)
+	    end
+	 
+            local function onChildAdded(child)
+                if table.find(entityNames, child.Name) then
+                    repeat
+                        task.wait()
+                    until plr:DistanceFromCharacter(child:GetPivot().Position) < 1000 or not child:IsDescendantOf(workspace)
+                    
+                    if child:IsDescendantOf(workspace) then
+                        notifyEntitySpawn(child)
+                    end
+                end
+            end
+
+            -- 无限循环以保持脚本运行并检查 hintrush 标志
+            local running = true
+            while running do
+                local connection = workspace.ChildAdded:Connect(onChildAdded)
+                
+                repeat
+                    task.wait(1) -- 根据需要调整等待时间
+                until not flags.hintrush or not running
+                
+                connection:Disconnect()
+            end 
+        else 
+            -- 关闭消息或进行其他清理（如有需要）
+            running = false
+        end
+    end
+})
+
+RightGroup:AddToggle('pe', {
+    Text = 'Item Event',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            _G.itemNotificationInstances = {}
+            local flags = {notifitems = true}
+
+            -- 发送通知的函数
+            local function notifyItem(itemName)
+                local itemMessage = "Item Event: " .. itemName .. " is spawned"
+                Library:Notify(itemMessage)
+            end
+
+            -- 监控新物品的出现
+            local function check(v)
+                if v:IsA("Model") and (v:GetAttribute("Pickup") or v:GetAttribute("PropType")) then
+                    task.wait(0.1)
+
+                    -- 尝试找到物品的主部件
+                    local part = v:FindFirstChild("Handle") or v:FindFirstChild("Prop") or v:FindFirstChildWhichIsA("BasePart")
+                    
+                    -- 如果找到了部件，发送通知
+                    if part then
+                        local itemName = v.Name
+                        notifyItem(itemName)
+                    end
+                end
+            end
+
+            -- 设置监视器以处理现有和新添加的物品
+            local function setup(room)
+                local assets = room:WaitForChild("Assets")
+
+                if assets then
+                    local subaddcon
+                    subaddcon = assets.DescendantAdded:Connect(function(v)
+                        check(v)
+                    end)
+
+                    for i, v in pairs(assets:GetDescendants()) do
+                        check(v)
+                    end
+
+                    task.spawn(function()
+                        repeat task.wait() until not flags.notifitems
+                        subaddcon:Disconnect()
+                    end)
+                end
+            end
+
+            local addconnect
+            addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                setup(room)
+            end)
+
+            for i, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room:FindFirstChild("Assets") then
+                    setup(room)
+                end
+            end
+
+            table.insert(_G.itemNotificationInstances, flags)
+
+        else
+            if _G.itemNotificationInstances then
+                _G.itemNotificationInstances = nil
+            end
+        end
+    end
+})
+
+RightGroup:AddToggle('pe', {
+    Text = 'Chat Enity / Item Event',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        local entityNames = {"RushMoving", "AmbushMoving", "Snare", "A60", "A120", "A90", "Eyes", "JeffTheKiller"}
+        local plr = game.Players.LocalPlayer
+
+        if state then
+            -- 实体生成消息发送函数
+            local function sendEntityMessage(entity)
+                local suffix = "stop watch entity spawned!!!"
+                local message = "Entity: " .. entity.Name:gsub("Moving", ""):lower() .. " " .. suffix
+                textChannel:SendAsync(message)
+            end
+
+            -- 监控实体生成
+            local function onChildAdded(child)
+                if table.find(entityNames, child.Name) then
+                    repeat task.wait() until plr:DistanceFromCharacter(child:GetPivot().Position) < 1000 or not child:IsDescendantOf(workspace)
+                    if child:IsDescendantOf(workspace) then
+                        sendEntityMessage(child)
+                    end
+                end
+            end
+
+            -- 物品生成消息发送函数
+            local function sendItemMessage(itemName)
+                local suffix = "is spawned in this time"
+                local message = "Item: " .. itemName .. " " .. suffix
+                textChannel:SendAsync(message)
+            end
+
+            -- 监控物品生成
+            local function check(v)
+                if v:IsA("Model") and (v:GetAttribute("Pickup") or v:GetAttribute("PropType")) then
+                    task.wait(0.1)
+                    local part = v:FindFirstChild("Handle") or v:FindFirstChild("Prop") or v:FindFirstChildWhichIsA("BasePart")
+                    if part then
+                        local itemName = v.Name
+                        sendItemMessage(itemName)
+                    end
+                end
+            end
+
+            local function setup(room)
+                local assets = room:WaitForChild("Assets")
+                if assets then
+                    local subaddcon = assets.DescendantAdded:Connect(function(v)
+                        check(v)
+                    end)
+                    for _, v in pairs(assets:GetDescendants()) do
+                        check(v)
+                    end
+                    task.spawn(function()
+                        repeat task.wait() until not state
+                        subaddcon:Disconnect()
+                    end)
+                end
+            end
+
+            -- 监听房间变化
+            local addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                setup(room)
+            end)
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room:FindFirstChild("Assets") then
+                    setup(room)
+                end
+            end
+
+            -- 监听实体生成
+            local connection = workspace.ChildAdded:Connect(onChildAdded)
+
+            -- 关闭时断开连接
+            task.spawn(function()
+                repeat task.wait(1) until not state
+                connection:Disconnect()
+                addconnect:Disconnect()
+            end)
         end
     end
 })
