28df002ce297294d831a8aba4a68be7e4bedc5b4
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-08-30 11:48:15 +0800

    Update Hydraulic Doors.lua

--- Hydraulic Doors.lua
+++ Hydraulic Doors.lua
@@ -1705,35 +1705,43 @@ MainGroup:AddToggle('pe', {
     Text = 'Nil Screech',
     Default = false,
     Tooltip = 'Walk through walls',
-    Callback = function(v)
-	flags.noscreech = val
+    Callback = function(val)
+        flags.noscreech = val
 
-		if val then
-			if not ScreechModule then ScreechModule = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules:FindFirstChild("Screech") end
-			ScreechModule.Parent = nil
-		else
-			ScreechModule.Parent = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules
-		end
-	end
+        if val then
+            if ScreechModule then
+                ScreechModule.Parent = nil
+            end
+        else
+            if ScreechModule then
+                ScreechModule.Parent = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules
+            end
+        end
+    end
 })
 buttons.noscreech = noscreechbtn
+
 local SpiderJumpscareModule = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules:FindFirstChild("SpiderJumpscare")
 MainGroup:AddToggle('pe', {
     Text = 'Nil Spider Jumpscare',
     Default = false,
     Tooltip = 'Walk through walls',
-    Callback = function(v)
-	flags.notimothy = val
+    Callback = function(val)
+        flags.notimothy = val
 
-		if val then
-			if not SpiderJumpscareModule then SpiderJumpscareModule = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules:FindFirstChild("SpiderJumpscare") end
-			SpiderJumpscareModule.Parent = nil
-		else
-			SpiderJumpscareModule.Parent = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules
-		end
-	end
+        if val then
+            if SpiderJumpscareModule then
+                SpiderJumpscareModule.Parent = nil
+            end
+        else
+            if SpiderJumpscareModule then
+                SpiderJumpscareModule.Parent = plr.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules
+            end
+        end
+    end
 })
 buttons.notimothy = notimothybtn
+
 RightGroup:AddLabel('---------------------')
 RightGroup:AddToggle('pe', {
     Text = 'Enity Event',
